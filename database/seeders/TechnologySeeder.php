6a6de7f815dc3a534a7ea742cef39c7cb52500ac
annaritadituri <annaritadituri@icloud.com>
2024-07-25 14:14:38 +0200

    [UPD] Seeded technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vue', 'Bootstrap', 'PHP', 'SQL', 'Laravel'];
+        foreach ($technologies as $technology) {
+            
+            $db_technology = new Technology();
+
+            $db_technology->name = $technology;
+            $db_technology->slug = Str::of($technology)->slug();
+
+            $db_technology->save();
+
+        }
+        
     }
 }
